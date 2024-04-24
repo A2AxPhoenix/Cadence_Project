457a18389d40c454635920064ab9a26a720e9077
Joshua Vargas <joshuavargasjnv@gmail.com>
2024-04-23 18:50:29 -0700

    profile page progress (from gurman)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,10 @@ import LandingPage from './pages/LandingPage/LandingPage';
 import Dashboard from './pages/Dashboard/Dashboard';
 import TestPage from './pages/TestPage/TestPage';
 import OnboardingPage from './pages/OnboardingPage/OnboardingPage';
+import Profile from './pages/Profile/Profile'; 
+//import EditProfile from './pages/Profile/Edit/Edit'; 
 import './style.css';
+//<Route path="/profile/edit" element={ <EditProfile />} />
 
 const App = () => {
   return (
@@ -13,6 +16,7 @@ const App = () => {
         <Route exact path="/" element={ <LandingPage />} />
         <Route path="/onboarding" element={ <OnboardingPage />} />
         <Route path="/dashboard" element={ <Dashboard />} />
+        <Route path="/profile" element={ <Profile />} />
         <Route path="/test" element={ <TestPage/>} />
       </Routes>
     </Router>
