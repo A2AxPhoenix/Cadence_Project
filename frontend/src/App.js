198a073ba143314f8e303c270c89496a892ff8e2
Joshua Vargas <joshuavargasjnv@gmail.com>
2024-04-25 21:01:41 -0700

    simon's messaging page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,11 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import TestPage from './pages/TestPage/TestPage';
 import OnboardingPage from './pages/OnboardingPage/OnboardingPage';
 import Profile from './pages/Profile/Profile'; 
+import MessagesPage from './pages/MessagesPage/MessagesPage';
 //import EditProfile from './pages/Profile/Edit/Edit'; 
 import './style.css';
+
+// This is only here because we cant seem to comment in the return
 //<Route path="/profile/edit" element={ <EditProfile />} />
 
 const App = () => {
@@ -16,6 +19,7 @@ const App = () => {
         <Route exact path="/" element={ <LandingPage />} />
         <Route path="/onboarding" element={ <OnboardingPage />} />
         <Route path="/dashboard" element={ <Dashboard />} />
+        <Route path="/messages" element={ <MessagesPage />} />
         <Route path="/profile" element={ <Profile />} />
         <Route path="/test" element={ <TestPage/>} />
       </Routes>
